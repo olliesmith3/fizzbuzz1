03cd690400a34eb5583842f5d01738fff7cab2cd
Connor Worthington <worthington.connor@gmail.com>
2020-08-25 12:50:32 +0100

    final tests added

--- lib/fizzbuzz1.rb
+++ lib/fizzbuzz1.rb
@@ -1,2 +1,11 @@
 def fizzbuzz1(x)
+  if x % 3 == 0 && x % 5 == 0
+    'fizzbuzz'
+  elsif x % 3 == 0
+    'fizz'
+  elsif x % 5 == 0
+    'buzz'
+  else
+    x
+  end
 end
--- spec/fizzbuzz1_rspec.rb
+++ spec/fizzbuzz1_rspec.rb
@@ -4,4 +4,13 @@ describe "fizzbuzz1" do
   it 'returns fizz when passed 3' do
     expect(fizzbuzz1(3)).to eq "fizz"
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz1(5)).to eq "buzz"
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz1(15)).to eq "fizzbuzz"
+  end
+  it 'returns x when passed 4' do
+    expect(fizzbuzz1(4)).to eq 4
+  end 
 end
